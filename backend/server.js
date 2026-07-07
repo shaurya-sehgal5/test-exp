a510b03055275f5bea6a77631c4ce4924d9e2d3d
Shaurya Sehgal <shauryasehgal555@gmail.com>
2026-07-08 03:40:06 +0530

    Update server to use dynamic port configuration

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,12 @@ const express = require("express");
 
 const app = express();
 
+const PORT = process.env.PORT || 8080;
+
 app.get("/", (req, res) => {
     res.send("Backend Running");
 });
 
-app.listen(3000);
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+});
