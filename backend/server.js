b893d8890348a5c7ad8037e1b781aefc3a09a660
Shaurya Sehgal <shauryasehgal555@gmail.com>
2026-07-08 03:57:29 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
     res.send("Backend Running");
