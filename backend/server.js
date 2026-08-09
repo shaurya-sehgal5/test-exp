cef39c0f5b83ef835bf85a6d07ee3dc093ef52c7
Shaurya Sehgal <shauryasehgal555@gmail.com>
2026-08-09 16:28:34 +0530

    Change health check endpoint to '/health'

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ app.get("/", (req, res) => {
     res.send("Backend Running");
 });
 
-app.get("/api/health", (req, res) => {
+app.get("/health", (req, res) => {
   res.status(200).json({
     status: "ok",
   });
